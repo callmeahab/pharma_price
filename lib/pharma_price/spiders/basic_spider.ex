b82c0ebdbd934e94f9dbb4e0a467013fe9719073
didlika <filipstamenkovic89@gmail.com>
2023-10-10 14:45:27 +0200

    - Add csv output file

--- lib/pharma_price/spiders/basic_spider.ex
+++ lib/pharma_price/spiders/basic_spider.ex
@@ -2,6 +2,8 @@ defmodule PharmaPrice.Spiders.SrboTradeScraper do
   alias PharmaPrice.Spiders.ScrapeItem
   use Crawly.Spider
 
+  @csv_file_path "/tmp/output.csv"
+
   @impl Crawly.Spider
   def base_url do
     "https://www.apotekasrbotrade.rs"
@@ -79,6 +81,20 @@ defmodule PharmaPrice.Spiders.SrboTradeScraper do
       :items => items,
       :requests => []
     }
+  end
+
+  defp append_items_to_csv(items) do
+    # Open the CSV file in append mode
+    {:ok, file} = File.open(@csv_file_path, [:append])
+
+    # Iterate through items and write them to the CSV file
+    Enum.each(items, fn item ->
+      csv_line = "#{item.title},#{item.price},#{item.link},#{item.image}\n"
+      File.write(file, csv_line)
+    end)
+
+    # Close the CSV file
+    File.close(file)
   end
 
   def build_absolute_url(url, request_url) do
